30d7376730eb03a791b0e5642b02192239716159
alcoranpaul <67833935+alcoranpaul@users.noreply.github.com>
2025-02-01 18:07:03 -0600

    Initialized Health Component

--- Source/Game/Character.cs
+++ Source/Game/Character.cs
@@ -14,14 +14,17 @@ public class Character : Script
 	[ShowInEditor, Serialize]
 	private MovementComponent.CameraArgs _cameraArgs;
 
-
+	[ShowInEditor, Serialize] private float startingHealth = 100f;
 
 	private MovementComponent _movementComponent;
+	private HealthComponent _healthComponent;
 
 	public override void OnAwake()
 	{
 		_movementComponent = new MovementComponent(_movementArgs, _cameraArgs, Actor.As<CharacterController>(), _movementArgs.CharacterObj.As
 		<AnimatedModel>());
+
+		_healthComponent = new HealthComponent(startingHealth);
 	}
 
 	public override void OnStart()
---
+++ Source/Game/HealthComponent.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using FlaxEngine;
+
+namespace Game;
+
+/// <summary>
+/// HealthComponent class.
+/// </summary>
+public class HealthComponent
+{
+	private float health;
+	private float maxHealth;
+	public float NormalizedHealth => health / maxHealth;
+
+	public event Action<float> OnHealthChanged;
+	public event EventHandler OnDeath;
+
+	public HealthComponent(float maxHealth)
+	{
+		this.maxHealth = maxHealth;
+		health = maxHealth;
+	}
+
+	public void Damage(float amount)
+	{
+		health -= amount;
+		if (health < 0)
+		{
+			health = 0;
+			OnDeath?.Invoke(this, EventArgs.Empty);
+		}
+		OnHealthChanged?.Invoke(health);
+	}
+
+	public void Heal(float amount)
+	{
+		health += amount;
+		if (health > maxHealth)
+			health = maxHealth;
+		OnHealthChanged?.Invoke(health);
+	}
+
+
+
+}
