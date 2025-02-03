30adfb5bf38af3be1826e9b6906bdf8c1e1940e9
alcoranpaul <67833935+alcoranpaul@users.noreply.github.com>
2025-02-02 23:10:12 -0600

    Added Explosion VFX

---
+++ Source/Game/Throwable.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using FlaxEngine;
+
+namespace Game;
+
+/// <summary>
+/// Throwable Script.
+/// </summary>
+public class Throwable : Script
+{
+	[ShowInEditor, Serialize] private Collider collider;
+	[ShowInEditor, Serialize] private Prefab vfxPrefab;
+
+	private bool firstCollision = true;
+	public override void OnStart()
+	{
+		collider.CollisionEnter += OnCollisionEnter;
+	}
+
+	public override void OnDisable()
+	{
+		collider.CollisionEnter -= OnCollisionEnter;
+		base.OnDisable();
+	}
+
+	private void OnCollisionEnter(Collision collision)
+	{
+		if (firstCollision)
+		{
+			firstCollision = false;
+			PrefabManager.SpawnPrefab(vfxPrefab, Actor.Position);
+		}
+	}
+
+}
