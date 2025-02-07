5cc7209d30ac7b6393371218d74188f43ca5b687
alcoranpaul <67833935+alcoranpaul@users.noreply.github.com>
2025-02-07 15:51:59 -0600

    Implemented Options UI
    - Added dancing enemies

---
+++ Source/Game/UI/MainMenuUI.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using FlaxEngine;
+using FlaxEngine.GUI;
+
+namespace Game;
+
+/// <summary>
+/// MainMenuUI Script.
+/// </summary>
+public class MainMenuUI : Script
+{
+	[ShowInEditor, Serialize] private UIControl playControl;
+	[ShowInEditor, Serialize] private UIControl quitControl;
+	[ShowInEditor, Serialize] private SceneReference gameScene;
+	private Button playBtn;
+	private Button quitBtn;
+
+	/// <inheritdoc/>
+	public override void OnStart()
+	{
+		playBtn = playControl.Get<Button>();
+		playBtn.Clicked += OnPlayClicked;
+		quitBtn = quitControl.Get<Button>();
+		quitBtn.Clicked += () => Engine.RequestExit();
+	}
+
+	private void OnPlayClicked()
+	{
+		Level.ChangeSceneAsync(gameScene);
+	}
+
+	/// <inheritdoc/>
+	public override void OnEnable()
+	{
+		// Here you can add code that needs to be called when script is enabled (eg. register for events)
+	}
+
+	/// <inheritdoc/>
+	public override void OnDisable()
+	{
+		playBtn.Clicked -= OnPlayClicked;
+		quitBtn.Clicked -= () => Engine.RequestExit();
+	}
+
+	/// <inheritdoc/>
+	public override void OnUpdate()
+	{
+		// Here you can add code that needs to be called every frame
+	}
+}
---
+++ Source/Game/UI/MenuOptions.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using FlaxEngine;
+using FlaxEngine.GUI;
+
+namespace Game;
+
+/// <summary>
+/// MenuOptions Script.
+/// </summary>
+public class MenuOptions : Script
+{
+	[ShowInEditor, Serialize] private UIControl loreControl;
+	[ShowInEditor, Serialize] private UIControl objectiveControl;
+	[ShowInEditor, Serialize] private UIControl optionsControl;
+
+	[ShowInEditor, Serialize] private UIControl loreContentControl;
+	[ShowInEditor, Serialize] private UIControl objectiveContentControl;
+	[ShowInEditor, Serialize] private UIControl optionsContentControl;
+
+	private Button loreButton;
+	private Button objectiveButton;
+	private Button optionsButton;
+
+	/// <inheritdoc/>
+	public override void OnStart()
+	{
+
+		loreButton = loreControl.Get<Button>();
+		objectiveButton = objectiveControl.Get<Button>();
+		optionsButton = optionsControl.Get<Button>();
+
+		loreContentControl.IsActive = false;
+		objectiveContentControl.IsActive = false;
+		optionsContentControl.IsActive = false;
+
+		loreButton.Clicked += OnLoreClicked;
+		objectiveButton.Clicked += OnObjectiveClicked;
+		optionsButton.Clicked += OnOptionsClicked;
+	}
+
+	private void OnOptionsClicked()
+	{
+		loreContentControl.IsActive = false;
+		objectiveContentControl.IsActive = false;
+		optionsContentControl.IsActive = !optionsContentControl.IsActive;
+	}
+
+
+	private void OnObjectiveClicked()
+	{
+		loreContentControl.IsActive = false;
+		objectiveContentControl.IsActive = !objectiveContentControl.IsActive;
+		optionsContentControl.IsActive = false;
+	}
+
+	private void OnLoreClicked()
+	{
+		loreContentControl.IsActive = !loreContentControl.IsActive;
+		objectiveContentControl.IsActive = false;
+		optionsContentControl.IsActive = false;
+	}
+
+
+	/// <inheritdoc/>
+	public override void OnDisable()
+	{
+
+		loreButton.Clicked -= OnLoreClicked;
+		objectiveButton.Clicked -= OnObjectiveClicked;
+		optionsButton.Clicked -= OnOptionsClicked;
+	}
+
+	/// <inheritdoc/>
+	public override void OnUpdate()
+	{
+		// Here you can add code that needs to be called every frame
+	}
+}
